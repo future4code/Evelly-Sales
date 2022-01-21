0159b30e9f0c94fb5217fa38646c797f20313c55
EvyLuanNatã <evellypsales@gmail.com>
2022-01-20 23:48:57 -0300

    salvando projeto

---
+++ modulo-atual/projetoRevisãoReact/site-sw/src/App.js
@@ -0,0 +1,18 @@
+import react {useState} from "react";
+import styled from "styled-components";
+import ContainerApp from "./CharacterDetailPage/styles";
+import { useState } from "react";
+
+// const CharacterDetailPage = (props) => {
+
+// const [currentPage, setcurrentPage] = useState(false)
+
+function App() {
+  return (
+    <ContainerApp>
+      <h1>Star Wars</h1>
+    </ContainerApp>
+  );
+}
+
+export default App;
---
+++ modulo-atual/projetoRevisãoReact/site-sw/src/CharacterDetailPage/styles.js
@@ -0,0 +1,11 @@
+import react from "react";
+import styled from "styled-components";
+
+const ContainerApp = styled.div `
+background-color: pink;
+align-items: center;
+display: column;
+
+`
+
+export default ContainerApp;
