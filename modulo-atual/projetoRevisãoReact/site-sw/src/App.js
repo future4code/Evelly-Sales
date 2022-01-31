872374d8912792e7ab25089585819794f1756e21
EvyLuanNatã <evellypsales@gmail.com>
2022-01-30 23:34:37 -0300

    salvando

--- modulo-atual/projetoRevisãoReact/site-sw/src/App.js
+++ modulo-atual/projetoRevisãoReact/site-sw/src/App.js
@@ -1,18 +1,43 @@
-import react {useState} from "react";
-import styled from "styled-components";
-import ContainerApp from "./CharacterDetailPage/styles";
-import { useState } from "react";
+import "./styles.css";
+import React from "react";
+import Login from "./pages/Login";
+import Home from "./pages/Home";
 
-// const CharacterDetailPage = (props) => {
+export default class App extends React.Component {
+  state = {
+    login: false
+  };
 
-// const [currentPage, setcurrentPage] = useState(false)
+  logar = () => {
+    this.setState({ login: true });
+  };
 
-function App() {
-  return (
-    <ContainerApp>
-      <h1>Star Wars</h1>
-    </ContainerApp>
-  );
+  logout = () => {
+    this.setState({ login: false });
+  };
+
+  render() {
+    // let pagina = <h1>Página Erro</h1>;
+
+    // if (this.state.login) {
+    //   pagina = <Home onClickLogout={this.logout} />;
+    // } else {
+    //   pagina = <Login onClickLogin={this.logar} />;
+    // }
+
+    let pagina = this.state.login ? (
+      <Home onClickLogout={this.logout} />
+    ) : (
+      <Login onClickLogin={this.logar} />
+    );
+
+    return (
+      <div className="App">
+        <h1>Star Wars</h1>
+        {/* <hr /> */}
+        {pagina}
+      </div>
+    );
+  }
 }
 
-export default App;
