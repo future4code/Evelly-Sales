7b70ff90a8743a91593876da97f08704d4bb9d86
unknown <evellypsales@gmail.com>
2022-02-24 23:50:36 -0300

    salvando segunda parte dos exercícios

--- modulo1/revisao-js/lista-revisao-js/lista-revisao-js/exercicios.js
+++ modulo1/revisao-js/lista-revisao-js/lista-revisao-js/exercicios.js
@@ -26,22 +26,41 @@ function retornaArrayOrdenado(array) {
 }
 
 // EXERCÍCIO 04
+
 function retornaNumerosPares(array) {
-    constPares = array
-    
-  
+let pares = []
+    for (let i = 0; i <= array.length-1; i++) {
+    if ( array [i] % 2 === 0) {
+    pares.push(array [i])  
+    }
+} 
+ return pares
 }
 
 // EXERCÍCIO 05
 function retornaNumerosParesElevadosADois(array) {
- 
+    let arrayParesElevados = retornaNumerosPares (array)
+    for (let i = 0; i <= arrayParesElevados.length-1; i++) {
+    arrayParesElevados [i] =  arrayParesElevados[i] * arrayParesElevados[i]
+
+    }
+
+ return arrayParesElevados
+    
 }
+
 
 // EXERCÍCIO 06
 function retornaMaiorNumero(array) {
-  
-}
-
+    // const arrayDeNumeros = array
+    let maior = -Infinity
+     for(let i = 0; i <= array.length-1; i++){
+         if (array [i] > maior){
+             maior = array [i];
+         }
+        }
+return maior
+    }
 // EXERCÍCIO 07
 function retornaObjetoEntreDoisNumeros(num1, num2) {
 
@@ -49,8 +68,17 @@ function retornaObjetoEntreDoisNumeros(num1, num2) {
 
 // EXERCÍCIO 08
 function retornaNPrimeirosPares(n) {
+  let numerosPares = [];
+  for (let i = 0; numerosPares.length < n; i++) {
+  if (i % 2 == 0) {
+  numerosPares.push(i);
+            }
+        }
+        return numerosPares;
+    }
+    
+    console.log(retornaNNumerosPares(3));
    
-}
 
 // EXERCÍCIO 09
 function classificaTriangulo(ladoA, ladoB, ladoC) {
@@ -64,12 +92,21 @@ function retornaSegundoMaiorESegundoMenor(array) {
 
 // EXERCÍCIO 11
 function retornaChamadaDeFilme(filme) {
-   
+    // const assistirFilme = filme
+   return `Venha assistir ao filme ${filme.nome}, de ${filme.ano}, dirigido por ${filme.diretor} e estrelado por ${filme.atores[0]}, ${filme.atores[1]}, ${filme.atores[2]}, ${filme.atores[3]}.`
+
 }
+
+// console.log ()
 
 // EXERCÍCIO 12
 function retornaPessoaAnonimizada(pessoa) {
-   
+ 
+    const usuario = {
+        ...pessoa,
+        nome: 'ANÔNIMO',
+    }
+    return usuario
 }
 
 // EXERCÍCIO 13A
